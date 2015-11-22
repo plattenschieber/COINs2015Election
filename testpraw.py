b616104a69e906cfa6bfcb311b4136382730c1b2
Jeronim Morina <plattenschieber@gmail.com>
2015-11-22 19:52:28 +0100

    get some insights on the praw objects

--- testpraw.py
+++ testpraw.py
@@ -1,2 +1,7 @@
 import praw
+from pprint import pprint
+
 r = praw.Reddit(user_agent='mac:COINs2015Election:v1.0.1 (by /u/plattenschieber)')
+subreddit = r.get_subreddit('SandersForPresident')
+for submission in subreddit.get_hot(limit=5):
+    pprint(help(submissions))
