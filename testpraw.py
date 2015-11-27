cf398952f9fe5ba718b60303c175ba23b29bae56
Jeronim Morina <plattenschieber@gmail.com>
2015-11-27 19:39:59 +0100

    reorder code and add a comment

--- testpraw.py
+++ testpraw.py
@@ -10,13 +10,14 @@ subreddit = r.get_subreddit('SandersForPresident')
 
 # get last 1000 comments in subreddit 'SandersForPresident'
 subreddit_comments = subreddit.get_comments(limit=1000)
-print('Number of comments including buzzwords: ', count)
 
 with open('comments.csv', 'w') as csvfile:
     # count total relevant answers
     count=0
     # flattening the tree since we don't care about the answering order
     flat_comments = praw.helpers.flatten_tree(subreddit_comments)
+
+    # print only some specific comments
     for comment in flat_comments:
         # test if it contains relevant information about a candidate
         buzzwords = ["sander", "bernie", "sanders"]
@@ -28,8 +29,10 @@ with open('comments.csv', 'w') as csvfile:
     # define dictionary for csv header and write it
     fieldnames = ['author', 'created_utc', 'subreddit', 'subreddit_id', 'ups', 'downs', 'body']
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-
     writer.writeheader()
+
     # get out all comments into the csv file
     for comment in flat_comments:
         writer.writerow({'author':comment.author, 'created_utc':comment.created_utc, 'subreddit':comment.subreddit, 'subreddit_id':comment.subreddit_id, 'ups':comment.ups, 'downs':comment.downs, 'body':comment.body.encode('utf-8')})
+
+print('Number of comments including buzzwords: ', count)
