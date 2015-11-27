da4991921cf7a2783c1ebf22053f4960e8745883
Jeronim Morina <plattenschieber@gmail.com>
2015-11-27 19:39:45 +0100

    move selection of comments into file handling

--- testpraw.py
+++ testpraw.py
@@ -10,20 +10,21 @@ subreddit = r.get_subreddit('SandersForPresident')
 
 # get last 1000 comments in subreddit 'SandersForPresident'
 subreddit_comments = subreddit.get_comments(limit=1000)
-# count total relevant answers
-count=0
-# flattening the tree since we don't care about the answering order
-flat_comments = praw.helpers.flatten_tree(subreddit_comments)
-for comment in flat_comments:
-    # test if it contains relevant information about a candidate
-    buzzwords = ["sander", "bernie", "sanders"]
-    if any(buzz in comment.body.lower() for buzz in buzzwords):
-        count = count + 1
-        # print only first 100 character (for more look at comment.body)
-        print(str(count) + ": " + str(comment))
 print('Number of comments including buzzwords: ', count)
 
 with open('comments.csv', 'w') as csvfile:
+    # count total relevant answers
+    count=0
+    # flattening the tree since we don't care about the answering order
+    flat_comments = praw.helpers.flatten_tree(subreddit_comments)
+    for comment in flat_comments:
+        # test if it contains relevant information about a candidate
+        buzzwords = ["sander", "bernie", "sanders"]
+        if any(buzz in comment.body.lower() for buzz in buzzwords):
+            count = count + 1
+            # print only first 100 character (for more look at comment.body)
+            print(str(count) + ": " + str(comment))
+
     # define dictionary for csv header and write it
     fieldnames = ['author', 'created_utc', 'subreddit', 'subreddit_id', 'ups', 'downs', 'body']
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
